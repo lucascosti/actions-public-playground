dc58f0e46f40487760974e40aa18eb74272f0455
Lucas Costi <lucascosti@github.com>
2019-11-22 14:05:09 +1000

    Basic node.js project

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();app.get('/', function (req, res) {
+  res.send('Hello World! Everything is awesome!!');
+});app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
