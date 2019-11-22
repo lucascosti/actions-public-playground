537652e2402016a34e17e1cb3fecd38fd3a83db7
Lucas Costi <lucascosti@github.com>
2019-11-22 15:07:05 +1000

    moar test revision

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ var app = express();app.get('/', function (req, res) {
 });app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+// export the app so we can include it in your tests.
+module.exports = app
--- test/test-page.js
+++ test/test-page.js
@@ -1,9 +1,21 @@
 var expect  = require('chai').expect;
 var request = require('request');
+// test/app.js
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+// `require` the exported `app`.
+const app = require('../app.js')
+
+const should = chai.should();
+chai.use(chaiHttp);
 
 it('Main page content', function(done) {
-    request('http://localhost:3000' , function(error, response, body) {
-        expect(body).to.equal('Hello World! Everything is awesome!!');
-        done();
-    });
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+            console.log(err); // outputs null
+            console.log(res.body) // {}
+            res.text.should.be.eql('Hello World! Everything is awesome!!'); // passes test
+            done();
+        });
 });
