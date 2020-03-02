0f34bf887658f4547a75a3ebc237dfa78ed2805d
Lucas Costi <lucascosti@github.com>
2020-03-02 12:46:18 +1000

    test remove password from client

--- client.js
+++ client.js
@@ -4,7 +4,6 @@ const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: 'postgres',
-    password: 'postgres',
     database: 'postgres'
 });
 
