5fb59ff7cac5f30ea3a72d090302f7b1aaf0fbc0
Lucas Costi <lucascosti@github.com>
2020-03-02 12:48:10 +1000

    Working container job

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: 'postgres',
+    password: 'postgres',
     database: 'postgres'
 });
 
